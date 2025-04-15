2ac29e705991e1a13069fe03f76b0690eab3c921
prospermbuma <prospermbuma@gmail.com>
2025-04-15 03:01:43 +0300

    Some updates

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
+<!-- ===============================================
+# SCRIPT
+=================================================-->
+<script setup lang="ts">
+const num: Number = 1;
+const name: String = 'Tanzania';
+</script>
+
+<!-- ===============================================
+# TEMPLATE
+=================================================-->
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="mt-20 flex justify-center items-center flex-col">
+    <div class="text-center bg-blue-50 rounded-2xl p-10 shadow-md">
+      <h1 class="text-4xl md:text-8xl">Hello, {{ name }}!</h1>
+      <p class="mt-2 text-xl text-blue-500">You are the number {{ num }}</p>
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
