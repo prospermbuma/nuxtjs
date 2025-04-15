6587db14c17b86a5ec4a1533ff62c0b0db028d38
prospermbuma <prospermbuma@gmail.com>
2025-04-15 03:25:45 +0300

    More changes

--- app.vue
+++ app.vue
@@ -10,6 +10,22 @@ const name: String = 'Tanzania';
 # TEMPLATE
 =================================================-->
 <template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/jobs">Jobs</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/contact">Contact</NuxtLink>
+      </li>
+    </ul>
+  </nav>
   <div class="mt-20 flex justify-center items-center flex-col">
     <div class="text-center bg-blue-50 rounded-2xl p-10 shadow-md">
       <h1 class="text-4xl md:text-8xl">Hello, {{ name }}!</h1>
